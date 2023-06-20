5baaf5d86f37eb9690033ab9e70d45a0296966a2
THENEWWKIDD <31018678+THENEWWKIDD@users.noreply.github.com>
2023-06-20 08:52:48 -0500

    first commit

---
+++ Flask_App/client.py
@@ -0,0 +1,13 @@
+import requests
+
+URL = "http://127.0.0.1:5000/record_and_store"
+TEST_AUDIO_FILE_PATH = "AudioFiles/Genesis 1_1.wav"
+
+if __name__ == "__main__":
+    
+    audio_file = open(TEST_AUDIO_FILE_PATH, "rb")
+    values = {"file": (TEST_AUDIO_FILE_PATH, audio_file, "audio/wav")}
+    response = requests.post(URL, files=values)
+    data = response.json()
+    
+    print(f"Your request is: {data['keyword']}")
